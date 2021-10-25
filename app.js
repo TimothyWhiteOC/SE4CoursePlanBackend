1455debb941addc9c96825f6d5c5a2b879e5960a
Timothy White <timothy.white@eagles.oc.edu>
2021-10-25 10:44:41 -0500

    Add semesters

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var studentCoursesRouter = require('./routes/student-courses');
 var degreesRouter = require('./routes/degrees');
 var degreeCoursesRouter = require('./routes/degree-courses');
 var advisorsRouter = require('./routes/advisors');
+var semestersRouter = require('./routes/semesters');
 
 var app = express();
 
@@ -45,6 +46,7 @@ app.use('/students', studentCoursesRouter);
 app.use('/degrees', degreesRouter);
 app.use('/degrees', degreeCoursesRouter);
 app.use('/advisors', advisorsRouter);
+app.use('/semesters', semestersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
